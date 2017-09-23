e215f5ed18a24a8572dd64135cae2c389c9e305e
Simon Nonnis <simononnis@gmail.com>
2017-09-23 11:56:29 +0100

    'Add flash msg on saving store'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -12,5 +12,6 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   const store = new Store(req.body);
   await store.save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect('/');
 }
