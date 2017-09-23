2200b8933147de3f557b642361d098edfd4f1c7a
Simon Nonnis <simononnis@gmail.com>
2017-09-23 20:34:52 +0100

    'Quering for stores and display them'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
-exports.homePage = (req, res) => {
-  res.render('index');
-};
-
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 };
@@ -14,4 +10,11 @@ exports.createStore = async (req, res) => {
 
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
-}
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query DB for a list of all stores.
+  const stores = await Store.find();
+
+  res.render('stores', { title: 'Stores', stores });
+};
