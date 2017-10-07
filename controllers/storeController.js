4cc0a4825dc0e0904bac0ea7da8b13284b9ae04e
Simon Nonnis <simononnis@gmail.com>
2017-10-07 17:13:48 +0100

    'Upload with multer and resize with jimp'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
 
 const multerOptions = {
   storage: multer.memoryStorage(),
   fileFilter(req, file, next) {
-    const isPhoto = file.mimetype.startsWidth('image/*');
+    const isPhoto = file.mimetype.startsWith('image/jpeg');
     if(isPhoto) {
       next(null, true);
     } else {
@@ -19,6 +21,21 @@ exports.addStore = (req, res) => {
 };
 
 exports.upload = multer(multerOptions).single('photo');
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    next();
+    return;
+  } else {
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // Now resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`publie/uploads/${req.body.photo}`);
+
+    next();
+  }
+};
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
