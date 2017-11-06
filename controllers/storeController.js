3866b23fc5be00dc7ea0573293f07defd1ffb645
Simon Nonnis <simononnis@gmail.com>
2017-11-06 20:03:46 +0000

    'Use Promise.all'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -84,7 +84,11 @@ exports.getStoreBySlug = async (req, res, next  ) => {
 }
 
 exports.getStoreByTag = async (req, res) => {
-  const tags = await Store.getTagsList();
   const tag = req.params.tag;
-  res.render('tag', { tags, title: 'Tags', tag });
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storePromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storePromise]);
+
+  res.render('tag', { tags, stores, title: 'Tags', tag });
 }
