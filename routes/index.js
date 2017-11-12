56245c14ca889272b50dcd02dce1ad17bc4e04fc
Simon Nonnis <simononnis@gmail.com>
2017-11-12 11:38:04 +0000

    'Create login form mixin'

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -21,6 +22,8 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoreByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
+
+router.get('/login', userController.loginForm);
 
 
 
