d28b1d7a3f9f62a21e5d1857699873547db47440
Simon Nonnis <simononnis@gmail.com>
2017-12-24 15:42:08 +0000

    'Relate a store to a user'

--- models/Store.js
+++ models/Store.js
@@ -34,6 +34,11 @@ const storeSchema = new mongoose.Schema({
   },
   photo: {
     type: String,
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an author',
   }
 });
 
