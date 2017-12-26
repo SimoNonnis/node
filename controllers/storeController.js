a1b196c3791e0c265032fb89e9893c7f11e8839d
Simon Nonnis <simononnis@gmail.com>
2017-12-26 19:04:02 +0000

    'imporve indexed query'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -102,11 +102,18 @@ exports.getStoreByTag = async (req, res) => {
 }
 
 exports.searchStores = async (req, res) => {
-  const stores = await Store.find({
+  const stores = await Store
+  .find({
     $text: {
       $search: req.query.q,
     }
-  });
+  }, {
+    score: { $meta: 'textScore'}
+  })
+  .sort({
+    score: { $meta: 'textScore'}
+  })
+  .limit(5);
 
   res.json(stores);
 }
