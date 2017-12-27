f54c484e6300ab43ac30d8cc8dbe790e6729b915
Simon Nonnis <simononnis@gmail.com>
2017-12-27 22:46:51 +0000

    'Hit search endpoint and get response'

---
+++ public/javascripts/modules/typeAhead.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+function typeAhead(search) {
+  if (!search) return;
+
+  const searchInput = search.querySelector('[name="search"]');
+  const searchResults = search.querySelector('.search__results');
+
+  searchInput.on('input', function() {
+    if (!this.value) {
+      searchResults.style.display = 'none';
+      return;
+    }
+
+    searchResults.style.display = 'block';
+    axios
+      .get(`/api/search?q=${this.value}`)
+      .then(res => {
+        if (res.data.length) {
+          console.log('Hey');
+        }
+      })
+  })
+}
+
+export default typeAhead;
