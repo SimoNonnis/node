a08b8e0b027fbd9b766507ab9d92204cbaa6b2b6
Simon Nonnis <simononnis@gmail.com>
2017-12-27 22:55:50 +0000

    'Remove this.value'

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -6,15 +6,15 @@ function typeAhead(search) {
   const searchInput = search.querySelector('[name="search"]');
   const searchResults = search.querySelector('.search__results');
 
-  searchInput.on('input', function() {
-    if (!this.value) {
+  searchInput.on('input', function(e) {
+    if (!e.target.value) {
       searchResults.style.display = 'none';
       return;
     }
 
     searchResults.style.display = 'block';
     axios
-      .get(`/api/search?q=${this.value}`)
+      .get(`/api/search?q=${e.target.value}`)
       .then(res => {
         if (res.data.length) {
           console.log('Hey');
