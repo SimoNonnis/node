0f9d12296c67793b5f3b7e4d015b7415d9b140ac
Simon Nonnis <simononnis@gmail.com>
2018-01-05 17:05:59 +0000

    'Build infowindow'

--- public/javascripts/modules/map.js
+++ public/javascripts/modules/map.js
@@ -8,13 +8,56 @@ const mapOptions = {
 };
 
 function loadPlaces(map, lat = 43.2, lng = -79.8) {
+  axios.get(`/api/stores/near?lat=${lat}&lng=${lng}`)
+    .then(res => {
+      const places = res.data;
+      if (!places.length) {
+        console.log('Nothing to return');
+        return;
+      }
 
+      // Create a bounds
+      const bounds = new google.maps.LatLngBounds();
+      const infoWindow = new google.maps.InfoWindow();
+
+      const markers = places.map(place => {
+        const [placeLng, placeLat] = place.location.coordinates;
+        const position = { lat: placeLat, lng: placeLng };
+        bounds.extend(position);
+        const marker = new google.maps.Marker({
+          map,
+          position,
+        });
+        marker.place = place;
+        return marker;
+      });
+
+      // When someone clicks on a marker, show details of that place
+      markers.forEach(marker => marker.addListener('click', function() {
+        const html = `
+          <div class="popup">
+            <a href="/store/${this.place.slug}">
+              <img src="/uploads/${this.place.photo || 'store.png'}" alt="${this.place.name}" />
+              <p>${this.place.name} - ${this.place.location.address}</p>
+            </a>
+          </div>
+        `;
+        infoWindow.setContent(html);
+        infoWindow.open(map, this);
+      }));
+
+      // Zoom the map to fit
+      map.setCenter(bounds.getCenter());
+      map.fitBounds(bounds);
+    })
 }
 
 function makeMap(mapDiv) {
   if (!mapDiv) return;
 
   const map = new google.maps.Map(mapDiv, mapOptions);
+  loadPlaces(map);
+
   const input = $('[name="geolocate"]');
   const autocomplete = new google.maps.places.Autocomplete(input);
 }
