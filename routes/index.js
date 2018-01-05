af90bcbb1693d369aa09d8e23bfb48766432590f
Simon Nonnis <simononnis@gmail.com>
2018-01-05 14:34:32 +0000

    'Handle map url'

--- routes/index.js
+++ routes/index.js
@@ -50,6 +50,8 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 
+router.get('/map', storeController.mapPage);
+
 // API
 router.get('/api/search/', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
