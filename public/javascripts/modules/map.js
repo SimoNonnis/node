709b990a14654c998312a60563c7b27a8f254e02
Simon Nonnis <simononnis@gmail.com>
2018-01-05 15:34:25 +0000

    'Make autocomplete'

---
+++ public/javascripts/modules/map.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { $ } from './bling';
+
+
+const mapOptions = {
+  center: { lat: 43.2, lng: -79.8},
+  zoom: 8,
+};
+
+function loadPlaces(map, lat = 43.2, lng = -79.8) {
+
+}
+
+function makeMap(mapDiv) {
+  if (!mapDiv) return;
+
+  const map = new google.maps.Map(mapDiv, mapOptions);
+  const input = $('[name="geolocate"]');
+  const autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+export default makeMap;
