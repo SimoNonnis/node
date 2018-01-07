a6105edca83a4515b0a7387596de5acea0f92c3d
Simon Nonnis <simononnis@gmail.com>
2018-01-07 13:14:35 +0000

    'Remove animation class after 2.5 s'

--- public/javascripts/modules/heart.js
+++ public/javascripts/modules/heart.js
@@ -8,7 +8,12 @@ function ajaxHeart(e) {
     .post(this.action)
     .then(res => {
       const isHearted = this.heart.classList.toggle('heart__button--hearted');
+
       $('.heart-count').textContent = res.data.hearts.length;
+      if (isHearted) {
+        this.heart.classList.add('heart__button--float');
+        setTimeout(()=> this.heart.classList.remove('heart__button--float'), 2500);
+      }
     })
     .catch( err => console.log(err))
 }
