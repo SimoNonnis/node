bc55e905abff93b8ab3c0dbb0fa79ca6a616a1bd
Simon Nonnis <simononnis@gmail.com>
2018-01-18 22:24:26 +0000

    'Make template to render stores by review'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -167,6 +167,6 @@ exports.getHearts = async (req, res) => {
 
 exports.getTopStores = async (req, res) => {
   const stores = await Store.getTopStores();
-  res.json(stores);
-  // res.render('topStores', { stores, title: 'Top stores!' });
+
+  res.render('topStores', { stores, title: 'Top stores!' });
 };
